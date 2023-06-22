7f8d15c64b4cb5d43d833a3ba373746358e9e6f6
Philipp Mendogralo <abkxj59@gmail.com>
2023-06-23 00:36:42 +0500

    Добавляет мобильную раскладку и слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,16 @@
 import {initMenu} from './modules/menu.js';
 import {initLanguageSelector} from './modules/language-selector.js';
 import {initAccordion} from './modules/accordion.js';
+import {addSlider} from './modules/slider.js';
+
 // // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
+  initMenu();
+  initLanguageSelector();
+  initAccordion();
+  addSlider();
+
   window.addEventListener('load', () => {
-    initMenu();
-    initLanguageSelector();
-    initAccordion();
   });
 });
