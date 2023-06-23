f95b82cdf9a6d17f9db3782914e0a828c319594e
Philipp Mendogralo <abkxj59@gmail.com>
2023-06-23 15:23:04 +0500

    one more bugfix

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -4,15 +4,11 @@ const slider = document.querySelector('.features__slider');
 const sliderWrapper = slider.querySelector('.features__list');
 const slides = slider.querySelectorAll('.features__item');
 
-slider.dataset.mobile = 'false';
-
 let mySwiper = null;
 
 const initSlider = () => {
   mySwiper = new Swiper('.swiper', {
     modules: [Pagination],
-
-    // autoHeight: true,
 
     pagination: {
       el: '.swiper-pagination',
@@ -22,29 +18,34 @@ const initSlider = () => {
   });
 };
 
-const addSlider = () => {
-  const checkMobile = () => {
-    if (window.innerWidth < 768 && slider.dataset.mobile === 'false') {
-      slider.dataset.mobile = 'true';
-      slider.classList.add('swiper');
-      sliderWrapper.classList.add('swiper-wrapper');
-      slides.forEach((slide) => {
-        slide.classList.add('swiper-slide');
-      });
-      initSlider();
-    } else if (window.innerWidth >= 768) {
-      slider.dataset.mobile = 'false';
-      slider.classList.remove('swiper');
-      sliderWrapper.classList.remove('swiper-wrapper');
-      slides.forEach((slide) => {
-        slide.classList.remove('swiper-slide');
-      });
+const checkMobile = () => {
+  if (window.innerWidth < 768 && slider.dataset.mobile === 'false') {
+    slider.dataset.mobile = 'true';
+    slider.classList.add('swiper');
+    sliderWrapper.classList.add('swiper-wrapper');
+    slides.forEach((slide) => {
+      slide.classList.add('swiper-slide');
+    });
+    initSlider();
+  } else if (window.innerWidth >= 768) {
+    slider.dataset.mobile = 'false';
+    slider.classList.remove('swiper');
+    sliderWrapper.classList.remove('swiper-wrapper');
+    slides.forEach((slide) => {
+      slide.classList.remove('swiper-slide');
+    });
+    if (mySwiper) {
       mySwiper.destroy();
     }
-  };
+  }
+};
 
-  checkMobile();
-  window.addEventListener('resize', checkMobile);
+const addSlider = () => {
+  if (slider && sliderWrapper && slides) {
+    slider.dataset.mobile = 'false';
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+  }
 };
 
 export {addSlider};
